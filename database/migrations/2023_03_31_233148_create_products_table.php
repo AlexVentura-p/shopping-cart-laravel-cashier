982b61fa7a2582a2ada5922864e0be1e1b22621b
AlexVentura-p <alex.ventura300@gmail.com>
2023-04-02 18:49:44 -0600

    Add basic model and migrations

--- database/migrations/2023_03_31_233148_create_products_table.php
+++ database/migrations/2023_03_31_233148_create_products_table.php
@@ -15,6 +15,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('description');
+            $table->text('description');
+            $table->decimal('price')->default(0);
             $table->timestamps();
         });
     }
