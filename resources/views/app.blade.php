22af71c5571d767004467c4a2736701a8078f273
AlexVentura-p <alex.ventura300@gmail.com>
2023-04-04 18:00:57 -0600

    Add blade main page

---
+++ resources/views/app.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Vue Stripe Cashier</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body class="antialiased">
+    <div class="container mx-auto">
+        <div id="app"></div>
+    </div>
+</body>
+</html>
